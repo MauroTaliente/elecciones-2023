cb9df7bcc1108937edbfe083083db645cbaccfe5
Mauro Taliente <maurotaliente@quiena.com>
2023-08-14 00:26:24 -0300

    styles improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default async function Home() {
 
   return (
     <main className="flex flex-col items-center justify-between p-y-24 gap-4">
-      <div className="flex flex-row gap-2 flex-wrap items-center justify-center">
+      <div className="flex flex-row gap-2 p-4 w-full flex-wrap items-center justify-center">
         <div className="flex flex-1 flex-col bg-slate-100 p-4 rounded-lg justify-center items-center">
           <label>Blancos</label>
           <h2><b>{formatNumber(blancos)}</b></h2>
@@ -56,8 +56,10 @@ export default async function Home() {
           <h2><b>{formatNumber(totalVotos)}</b></h2>
         </div>
       </div>
-      <div className="p-4 w-full max-w-lg bg-slate-900 rounded-lg">
-        <Graph partidos={partidosOr} />
+      <div className="flex px-4 w-full">
+        <div className="flex p-4 w-full bg-slate-900 rounded-lg max-h-[640px] justify-center items-center">
+          <Graph partidos={partidosOr} />
+        </div>
       </div>
       <div className="flex flex-row flex-wrap">
         {partidosOr
@@ -79,30 +81,32 @@ export default async function Home() {
             })();
 
             const src = (() => {
-              if (code === '135') return 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Logo_La_Libertad_Avanza_%28cropped%29.png';
+              if (code === '135') return 'https://upload.wikimedia.org/wikipedia/commons/6/6a/Logo_La_Libertad_Avanza.png';
               if (code === '134') return 'https://upload.wikimedia.org/wikipedia/commons/c/c4/Frente_de_Todos_logo.svg';
               if (code === '132') return 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Juntos-Por-El-Cambio-Logo.svg/2880px-Juntos-Por-El-Cambio-Logo.svg.png';
               return 'https://www.electoral.gob.ar/nuevo/img/btnPrincipal/icono_1.png';
             })();
 
             return (
-              <div key={code} className="flex py-2 md:p-4 w-full md:w-1/2">
+              <div key={code} className="flex py-2 md:p-4 w-full md:w-1/2 lg:w-1/3">
                 <div className="flex flex-col bg-slate-300 justify-start items-centerw-full rounded-lg overflow-hidden w-full">
-                  <header className={`flex flex-row justify-between items-start p-4 gap-2 min-h-40 ${bg}`}>
-                    <h4 className="flex flex-1" >{name}</h4>
-                    <div className="bg-white p-2 rounded-md">
-                      <Image
-                        src={src}
-                        width={120}
-                        height={120}
-                        alt={name}
-                      />
+                  <header className={`flex  flex-col sm:flex-row flex-wrap justify-between items-start p-4 gap-2 min-h-40 ${bg}`}>
+                    <h5 className="sm:flex sm:flex-1" ><b>{name}</b></h5>
+                    <div className="bg-white rounded-md p-4">
+                      <div className="w-[120px] h-[80px] relative">
+                        <Image
+                          fill
+                          src={src}
+                          alt={name}
+                          style={{objectFit: "contain"}}
+                        />
+                      </div>
                     </div>
                   </header>
                   <div className="flex flex-col p-4">
-                    <div className="flex flex-col md:flex-row justify-stretch items-stretch">
-                      <h2 className="w-full md:w-1/2"><b>%: {perc}</b></h2>
-                      <h2 className="w-full md:w-1/2">T: {formatNumber(votos)}</h2>
+                    <div className="flex flex-col justify-stretch items-stretch">
+                      <h2 className="w-full"><b>%: {perc}</b></h2>
+                      <h2 className="w-full">T: {formatNumber(votos)}</h2>
                     </div>
                     {/* <ul>
                     <li className="flex flex-col md:flex-row justify-stretch items-stretch">
